c32cf501c8884ec3fb588fbe8e0c2b045faa8750
jayaboddeda <38948963+jayaboddeda@users.noreply.github.com>
2021-04-25 19:30:17 +0530

    added cloud mongodb atlas

--- src/backend/databases/sqlite.js
+++ src/backend/databases/sqlite.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/userRegistration",{
+mongoose.connect("mongodb+srv://tvastra:tvastra@tvastracluster.dc40i.mongodb.net/userRegistration?retryWrites=true&w=majority",{
   useNewUrlParser:true,
   useCreateIndex:true,
   useUnifiedTopology:true
